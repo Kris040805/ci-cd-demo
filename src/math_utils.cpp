9bb4d0e3338ad0dced22d2e65762851fc49970ab
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2026-01-14 17:52:31 +0200

    static analysis fail

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -3,20 +3,20 @@
 
 int add(int a, int b)
 {
-    // return a + b;
+    return a + b;
 
     // test fail
-    return a - b;
+    // return a - b;
 }
 
 double divide(int a, int b)
 {
-    if (b == 0)
-    {
-        throw std::invalid_argument("Division by zero");
-    }
-    return static_cast<double>(a) / b;
+    // if (b == 0)
+    // {
+    //     throw std::invalid_argument("Division by zero");
+    // }
+    // return static_cast<double>(a) / b;
 
     // static analysis fail
-    // return a / 0;
+    return a / 0;
 }
