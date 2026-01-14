6fb3d85824ccc226dd299911f68b77176300c0bd
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2026-01-14 14:46:12 +0200

    wrong formatting

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -1,17 +1,15 @@
 #include "math_utils.hpp"
 #include <stdexcept>
 
-int add(int a, int b)
-{
-    return a + b;
+int add(int a, int b) { return a + b;
     // return a - b;
 }
 
 double divide(int a, int b)
 {
-    // if (b == 0)
-    // {
-    //     throw std::invalid_argument("Division by zero");
-    // }
+    if (b == 0)
+    {
+        throw std::invalid_argument("Division by zero");
+    }
     return static_cast<double>(a) / b;
 }
