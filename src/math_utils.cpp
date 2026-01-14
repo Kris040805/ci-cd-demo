237c659c2eb4e976c99926905af0f357db5c8e4f
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2026-01-14 17:54:04 +0200

    formatting fail

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -1,22 +1,23 @@
 #include "math_utils.hpp"
 #include <stdexcept>
 
-int add(int a, int b)
-{
+int add(int a, int b) {
     return a + b;
 
+
+
     // test fail
-    // return a - b;
-}
+    // return a - b; 
+    }
 
 double divide(int a, int b)
 {
-    // if (b == 0)
-    // {
-    //     throw std::invalid_argument("Division by zero");
-    // }
-    // return static_cast<double>(a) / b;
+    if (b == 0)
+    {
+        throw std::invalid_argument("Division by zero");
+    }
+    return static_cast<double>(a) / b;
 
     // static analysis fail
-    return a / 0;
+    // return a / 0;
 }
